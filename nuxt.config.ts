6d34673a1d170ad26004d2ca884024d58f557149
Yuryd <yurydevru@gmail.com>
2025-06-17 00:06:49 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+export default defineNuxtConfig({
+
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true }
+})
+function defineNuxtConfig(config: { compatibilityDate: string; devtools: { enabled: boolean; }; }) {
+  return config;
+}
+
