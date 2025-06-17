1d73561fd199af52b575100ef17ad363fd7dab5d
Yuryd <yurydevru@gmail.com>
2025-06-17 02:05:39 +0200

    chore: update devDependencies to include new packages for security, PWA support, and sitemap generation

---
+++ app/layouts/default.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+    
+</script>
+
+<template>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio fugit quaerat expedita quia! Magnam eius, aperiam sunt veritatis facere nobis.</div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+    definePageMeta({
+        title: 'Home Page',
+        layout: 'default',
+    })
+</script>
+
+<template>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, iure.</div>
+</template>
